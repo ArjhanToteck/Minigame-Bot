e738697a7a7c7fe8259934ccbe65a19df13e5034
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-01-31 22:54:46 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -474,8 +474,8 @@ client.on('message', message => {
 							while(bsGames[message.author.id].botKnowledge.misses.includes(coordinates)){
 								coordinates = [(Math.floor(Math.random() * Math.floor(10)) + 1), letters[Math.floor(Math.random() * Math.floor(letters.length))]];
 							}
-						} else if(bsGames[message.author.id].botKnowledge.hits[Object.keys(bsGames[message.author.id].botKnowledge.hits)[bject.keys(bsGames[message.author.id].botKnowledge.hits).length - 1]].nextFound == false){
-							var hitShip = Object.keys(bsGames[message.author.id].botKnowledge.hits)[bject.keys(bsGames[message.author.id].botKnowledge.hits).length - 1];
+						} else if(bsGames[message.author.id].botKnowledge.hits[Object.keys(bsGames[message.author.id].botKnowledge.hits)[Object.keys(bsGames[message.author.id].botKnowledge.hits).length - 1]].nextFound == false){
+							var hitShip = Object.keys(bsGames[message.author.id].botKnowledge.hits)[Object.keys(bsGames[message.author.id].botKnowledge.hits).length - 1];
 
 							var i = 0;
 							while(bsGames[message.author.id].botKnowledge.misses.includes(coordinates)){
