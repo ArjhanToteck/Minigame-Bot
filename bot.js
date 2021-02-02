bcf13fd28b5fa252397ab6858921a2e866881c7c
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-02-01 19:44:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -354,11 +354,7 @@ client.on('message', message => {
 							message.delete(1);
 							bsGames[message.author.id].editable.edit(generateEmbed("Battleship", "\n \n Preview of my board: (remember, :x: means miss and :white_check_mark: means hit): \n \n" + stringifyArray(bsGames[message.author.id].botPreview) + "\n \n Use `(number)(letter)` to attack your enemy. For example, `1a` will attack the position 1a on the enemy map."));
 						} else {
-							// (number)(letter)
-							if (message.content == "ok"){
-								message.delete(1);
-								bsGames[message.author.id].editable.edit(generateEmbed("Battleship", "\n \n Preview of my board: (remember, :x: means miss and :white_check_mark: means hit): \n \n" + stringifyArray(bsGames[message.author.id].botPreview) + "\n \n Use `(number)(letter)` to attack your enemy. For example, `1a` will attack the position 1a on the enemy map."));
-							} else if ((message.content.length == 2 || message.content.length == 3)) {
+							if ((message.content.length == 2 || message.content.length == 3) && message.content != "ok") {
 								message.delete(1);
 	
 								var coordinates = ["", ""];
@@ -459,7 +455,11 @@ client.on('message', message => {
 										bsGames[message.author.id].turn = "bot";
 										output += "\n \n Preview of my board (remember, :x: means miss and :white_check_mark: means hit): \n \n" + stringifyArray(bsGames[message.author.id].botPreview) + "\n \n Type `ok` to continue.";
 									} else {
-										output += "\n \n Type `ok` to contrinue and then try again by using `(number)(letter)` to attack your enemy. For example, `1a` will attack the position 1a on the enemy map."
+										if(!message.content == "ok"){
+											output += "\n \n Type `ok` to contrinue and then try again by using `(number)(letter)` to attack your enemy. For example, `1a` will attack the position 1a on the enemy map.";	
+										} else {
+											bsGames[message.author.id].turn = "bot";
+										}
 									}
 								}
 	
